b6c0ff6d6bb9d3b6d975c3961ca57720e9d5f3c2
Márton Braun <marton.braun@jetbrains.com>
2024-05-09 23:08:43 +0200

    Checkpoint 6

--- data/storage/src/MuseumStorage.kt
+++ data/storage/src/MuseumStorage.kt
@@ -1,8 +1,6 @@
 package com.jetbrains.kmpapp.data
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.map
 
 interface MuseumStorage {
     suspend fun saveObjects(newObjects: List<MuseumObject>)
@@ -12,18 +10,3 @@ interface MuseumStorage {
     fun getObjects(): Flow<List<MuseumObject>>
 }
 
-class InMemoryMuseumStorage : MuseumStorage {
-    private val storedObjects = MutableStateFlow(emptyList<MuseumObject>())
-
-    override suspend fun saveObjects(newObjects: List<MuseumObject>) {
-        storedObjects.value = newObjects
-    }
-
-    override fun getObjectById(objectId: Int): Flow<MuseumObject?> {
-        return storedObjects.map { objects ->
-            objects.find { it.objectID == objectId }
-        }
-    }
-
-    override fun getObjects(): Flow<List<MuseumObject>> = storedObjects
-}

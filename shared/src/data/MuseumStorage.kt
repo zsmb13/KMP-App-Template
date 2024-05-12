94ccf05245def8d7ca701d4f52efa679c592d3cb
Márton Braun <marton.braun@jetbrains.com>
2024-05-12 18:53:29 +0200

    Add dispatcher

--- shared/src/data/MuseumStorage.kt
+++ shared/src/data/MuseumStorage.kt
@@ -1,8 +1,11 @@
 package com.jetbrains.kmpapp.data
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 
 interface MuseumStorage {
     suspend fun saveObjects(newObjects: List<MuseumObject>)
@@ -15,7 +18,10 @@ interface MuseumStorage {
 class InMemoryMuseumStorage : MuseumStorage {
     private val storedObjects = MutableStateFlow(emptyList<MuseumObject>())
 
-    override suspend fun saveObjects(newObjects: List<MuseumObject>) {
+    private val dispatcher =
+        Dispatchers.IO.limitedParallelism(1)
+
+    override suspend fun saveObjects(newObjects: List<MuseumObject>) = withContext(dispatcher) {
         storedObjects.value = newObjects
     }
 

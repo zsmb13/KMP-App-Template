b55dc186667dfb2d30532734c9cdd88c4b50b021
Márton Braun <marton.braun@jetbrains.com>
2024-05-17 18:23:25 +0200

    Bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,8 +17,6 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/amper/amper")
         maven("https://www.jetbrains.com/intellij-repository/releases")
         maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
-        // Amper dev repo
-        maven("https://packages.jetbrains.team/maven/p/amper/amper")
     }
 }
 
@@ -36,5 +34,5 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("org.jetbrains.amper.settings.plugin").version("0.3.0-dev-559")
+    id("org.jetbrains.amper.settings.plugin").version("0.3.0")
 }
